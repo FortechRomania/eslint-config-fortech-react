84034b6247a0853e9859897d6d68923a4fd251c8
alexnm <alex.n.moldovan@gmail.com>
2017-10-09 10:19:36 +0300

    initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": [ "airbnb", "fortech" ],
+    "parser": "babel-eslint",
+    "rules": {
+        "jsx-quotes": 2,
+        "react/jsx-curly-spacing": [ 2, "always" ],
+        "react/jsx-indent": [ 2, 4 ],
+        "react/jsx-filename-extension": 0,
+        "react/jsx-indent-props": [ 2, 4 ]
+    }
+};
