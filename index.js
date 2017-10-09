c5b19f8e077789ac33fed83a206381c209f62e44
alexnm <alex.n.moldovan@gmail.com>
2017-10-09 10:57:01 +0300

    added jsx link exception

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ module.exports = {
         "react/jsx-curly-spacing": [ 2, "always" ],
         "react/jsx-indent": [ 2, 4 ],
         "react/jsx-filename-extension": 0,
-        "react/jsx-indent-props": [ 2, 4 ]
+        "react/jsx-indent-props": [ 2, 4 ],
+        "jsx-a11y/anchor-is-valid": [ "error", {
+            "components": [ ],
+            "specialLink": [ "hrefLeft", "hrefRight" ],
+            "aspects": [ "noHref", "invalidHref", "preferButton" ]
+        }]
     }
 };
